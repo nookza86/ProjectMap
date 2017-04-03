cd02b58fcc21d965cb9cf7256c077cb9ef528dc0
nookza86 <nook_we@hotmail.com>
2017-04-03 21:10:21 +0700

    Login & Register

---
+++ android_login_api/include/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "id1128718_dbadmin");
+define("DB_PASSWORD", "123456789");
+define("DB_DATABASE", "id1128718_mapofmem");
+?>
